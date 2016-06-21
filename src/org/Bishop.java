78fda2c418df75665a0267194bb178f927236b91
gpaulbr <gpaulbr@github.com>
2016-06-21 18:59:11 -0300

    adds basic bishop movement

--- src/org/Bishop.java
+++ src/org/Bishop.java
@@ -5,6 +5,16 @@ public class Bishop extends Piece {
 	public Bishop(String color) {
 		super(color);
 	}
+	
+	@Override
+	public boolean validateMove(int fromX, int fromY, int toX, int toY){
+		if (java.lang.Math.abs(fromX - toX) == java.lang.Math.abs(fromY - toY)) {
+			return true;
+		}
+		
+		return false;
+	}
+	
 	@Override
 	public String toString(){
 		return "B";
--- src/org/Rook.java
+++ src/org/Rook.java
@@ -5,6 +5,7 @@ public class Rook extends Piece {
 	public Rook(String color) {
 		super(color);
 	}
+	
 	@Override
 	public boolean validateMove(int fromX, int fromY, int toX, int toY){
 		if (fromX != toX && fromY != toY){
